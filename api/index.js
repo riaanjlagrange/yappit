260bb29a9f285a5415c978de8f2a144e8c2836e7
riaanjlagrange <riaanjlagrange@gmail.com>
2026-01-03 18:03:50 +0200

    add: Trust proxy

--- api/index.js
+++ api/index.js
@@ -15,6 +15,7 @@ app.use(cors());
 app.use(cookieParser());
 
 // Apply to all requests
+app.set('trust proxy', 1);
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
